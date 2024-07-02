69ded39367a37f9d74e02bc34786d0a1be3dbc26
Jay Tailor <jay8460568088@gmail.com>
2024-07-02 15:01:50 +0530

    Fixed home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  router.push('/dashboard');
   return (
+
     <div>
-      Hello world
-      <Button>Subscribe</Button>
+      
     </div>
   );
 }
